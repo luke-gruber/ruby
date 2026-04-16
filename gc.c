2f1dad37aadb1d8b06db104a329519e7c48f3186
Luke Gruber <luke.gruber@shopify.com>
2026-04-16 18:15:09 -0400

    psweep: Add assume in gc.c

--- gc.c
+++ gc.c
@@ -2501,7 +2501,9 @@ obj_free_object_id(VALUE obj, bool in_user_gc_thread)
 bool
 rb_gc_obj_free_concurrency_safe_vm_weak_references(VALUE obj)
 {
+    ASSUME(!RB_SPECIAL_CONST_P(obj));
     bool result = obj_free_object_id(obj, false);
+
     if (RB_UNLIKELY(rb_obj_gen_fields_p(obj))) {
         bool freed_generic = rb_free_generic_ivar(obj);
         if (!freed_generic) result = false;
